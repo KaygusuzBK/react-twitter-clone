96ad9ab2770b741e18bb20aef1c295f7e69d0670
Berkan Kaygusuz <berkankaygusuz41@outlook.com>
2023-10-09 01:24:33 +0300

    Subscribe butonu

--- src/layouts/main/rightbar/searchBar/index.jsx
+++ src/layouts/main/rightbar/searchBar/index.jsx
@@ -1,15 +1,21 @@
+import classNames from "classnames";
 import { FaSearch } from "react-icons/fa";
 function SearchBar() {
   return (
     <>
-      <div className="rounded-3xl bg-zinc-900 bg-opacity-60 flex items-center mt-2 p-2 w-full">
+      <div className="rounded-full bg-zinc-800 bg-opacity-90 flex items-center mt-2 p-2 w-full focus-within:bg-black focus-within:border focus-within:border-blue-500  focus-within:ring-blue-500 focus-within:ring-opacity-50 absolutetop-0 absoluteleft-0 z-10">
         <button>
-          <FaSearch className="text-gray-400 text-lg m-1" />
+          <FaSearch
+            className={classNames(
+              "text-gray-400 text-lg m-1",
+              "hover:text-blue-500"
+            )}
+          />
         </button>
         <input
           type="text"
           placeholder="Ara"
-          className="bg-transparent outline-none pl-1 ml-2 w-full "
+          className="bg-transparent outline-none pl-1 ml-2 w-full"
         />
       </div>
     </>
--- src/layouts/main/rightbar/subscribe/index.jsx
+++ src/layouts/main/rightbar/subscribe/index.jsx
@@ -13,7 +13,7 @@ function Subscribe() {
   }
   return (
     <>
-      <div className="bg-zinc-900 rounded-2xl mt-3">
+      <div className="bg-zinc-900 bg-opacity-85 rounded-2xl mt-3">
         <div className="p-4 -ml-1 ">
           <div>
             <div className=" text-xl font-bold ">Premium'a Abone Ol</div>
@@ -43,7 +43,7 @@ function Subscribe() {
                   leaveFrom="opacity-100"
                   leaveTo="opacity-0"
                 >
-                  <div className="fixed inset-0 bg-black bg-opacity-25" />
+                  <div className="fixed inset-0 bg-gray-600 bg-opacity-40" />
                 </Transition.Child>
 
                 <div className="fixed inset-0 overflow-y-auto">
@@ -57,30 +57,76 @@ function Subscribe() {
                       leaveFrom="opacity-100 scale-100"
                       leaveTo="opacity-0 scale-95"
                     >
-                      <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                      <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-black p-6 text-left align-middle shadow-xl transition-all">
                         <Dialog.Title
                           as="h3"
-                          className="text-lg font-medium leading-6 text-gray-900"
+                          className="text-2xl font-bold leading-10 text-white justify-center text-center pt-4"
                         >
-                          Payment successful
+                          Hesabının türü hangisi?
                         </Dialog.Title>
-                        <div className="mt-2">
-                          <p className="text-sm text-gray-500">
-                            Your payment has been successfully submitted. We’ve
-                            sent you an email with all of the details of your
-                            order.
-                          </p>
-                        </div>
+                        <div className="justify-center text-center p-4">
+                          <div className="mt-2 p-4">
+                            <p className="text-sm text-white justify-center text-center">
+                              Sana uygun doğru aboneliği seç:
+                            </p>
+                          </div>
 
-                        <div className="mt-4">
-                          <button
-                            type="button"
-                            className="inline-flex justify-center rounded-full
-                             border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                            onClick={closeModal}
-                          >
-                            Got it, thanks!
-                          </button>
+                          <div className=" flex justify-around p-4">
+                            <button
+                              type="button"
+                              className="justify-center items-center
+                              bg-black m-2 py-6 px-2 text-sm font-medium text-white hover:bg-gray-800  focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 border border-blue-500 rounded-lg w-1/2"
+                              onClick={closeModal}
+                            >
+                              <div className="text-gray-400 justify-start text-left">
+                                <small>Premium</small>
+                                <p className="text-white">Bireyim</p>
+                                <small>
+                                  Bireyler ve içerik üreticileri için
+                                </small>
+                              </div>
+                            </button>
+                            <button
+                              type="button"
+                              className="inline-flex justify-center rounded-lg
+                             border border-transparent bg-black m-2 py-6 px-2 text-sm font-medium text-white hover:bg-gray-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 w-1/2"
+                              onClick={closeModal}
+                            >
+                              <div className="text-gray-400 justify-start text-left">
+                                <small>Onaylı Kuruluşlar</small>
+                                <p className="text-white">Kuruluşum</p>
+                                <small>
+                                  İşletmeler, devlet kurumları, kar amacı
+                                  gütmeyen kuruluşlar için
+                                </small>
+                              </div>
+                            </button>
+                          </div>
+                          <div className="mt-4">
+                            <button className="text-black bg-white w-full rounded-full p-4 font-bold hover:bg-gray-200">
+                              Abone ol
+                            </button>
+                          </div>
+                          <div className="mt-4">
+                            <p>
+                              Daha fazla bilgi al:{" "}
+                              <a
+                                href="https://help.twitter.com/tr/using-x/x-premium"
+                                target="_blank"
+                                className="text-blue-500"
+                              >
+                                Premium
+                              </a>{" "}
+                              ve{" "}
+                              <a
+                                href="https://help.twitter.com/tr/using-x/verified-organizations"
+                                target="_blank"
+                                className="text-blue-500"
+                              >
+                                Onaylı Kuruluşlar
+                              </a>
+                            </p>
+                          </div>
                         </div>
                       </Dialog.Panel>
                     </Transition.Child>
