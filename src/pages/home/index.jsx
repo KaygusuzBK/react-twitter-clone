bfdb8163551f40a243377a503741fad9d9bb089f
Berkan Kaygusuz <berkankaygusuz41@outlook.com>
2023-10-16 02:10:56 +0300

    Sanallaştırma "çalışma"

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,8 @@ import WhereIsThis from "~/components/whereIsThis";
 import PostChoice from "./postChoice";
 import TwitForm from "~/components/twitForm";
 import { useSelector } from "react-redux";
+import { VList } from "virtua";
+import { WVList } from "virtua";
 
 export default function Home() {
   const { accounts } = useSelector((state) => state.auth);
@@ -15,18 +17,22 @@ export default function Home() {
       <div>
         <PostChoice />
       </div>
-      <div className="">
-        {accounts.map((account) =>
-          account.info.tweets.map((twit) => (
-            <TwitForm
-              key={twit.id}
-              ımage={account.avatar}
-              fullName={account.fullName}
-              username={account.username}
-              twit={twit}
-            />
-          ))
-        )}
+      <div style={{ width: "100%", height: "100vh" }}>
+        <WVList>
+          {accounts.map((account) =>
+            account.info.tweets.map((twit) => (
+              <div key={twit.id}>
+                <TwitForm
+                  key={twit.id}
+                  ımage={account.avatar}
+                  fullName={account.fullName}
+                  username={account.username}
+                  twit={twit}
+                />
+              </div>
+            ))
+          )}
+        </WVList>
       </div>
     </>
   );
